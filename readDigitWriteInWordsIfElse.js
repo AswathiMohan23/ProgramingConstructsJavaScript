fc96ad34756fb6a2eb8d7356a0cc9605b9ce69e3
AswathiMohan23 <achu999here@gmail.com>
2022-12-16 02:49:03 +0530

    [ADDED] : code to read digit and write in words using if else

---
+++ readDigitWriteInWordsIfElse.js
@@ -0,0 +1,34 @@
+const input=process.argv;
+const argv=input.slice(2);
+const number=parseInt(argv[0]);
+
+if(number==1)
+    console.log("One");
+else if(number==3)
+    console.log("two");
+            
+else if(number==4)
+    console.log("three");
+           
+else if(number==4)
+    console.log("four");
+
+else if(number==5)
+    console.log("five");
+
+else if(number==6)       
+    console.log("six");
+           
+else if(number==7) 
+    console.log("seven");
+
+else if(number==8)
+    console.log("eight");
+
+else if(number==9)      
+    console.log("nine");
+
+else if(number==10)           
+    console.log("ten");
+                    
+
