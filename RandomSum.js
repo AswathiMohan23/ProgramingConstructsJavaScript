2c270224932248c6a27bf62069ca2a85c031fd18
AswathiMohan23 <achu999here@gmail.com>
2022-12-16 01:11:13 +0530

    [ADDED] :  code to find sum of random numbers

---
+++ RandomSum.js
@@ -0,0 +1,5 @@
+let firstNo=Math.floor(Math.random() * 10);
+let secondNo=Math.floor(Math.random() * 10);
+let sum=firstNo+secondNo;
+console.log("the nos are :  "+firstNo+" and "+secondNo);
+console.log("Sum of Random numbers = "+sum);
