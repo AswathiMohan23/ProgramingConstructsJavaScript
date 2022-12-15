f0bab92524721cab4e94346630e1909cc766fdd1
AswathiMohan23 <achu999here@gmail.com>
2022-12-16 01:54:06 +0530

    [ADDED] : code to generate random array and find the repeating elements in that array

--- RepeatingNumbers.js
+++ RepeatingNumbers.js
@@ -1,19 +1,12 @@
 const randomArray=new Array;
 const repeatingArray=new Array;
-for(let i=0;i<20;i++){
+const outputArray=new Array;
+
+for(let i=0;i<=20;i++){
     randomArray[i]=Math.floor(Math.random() * 100);
     console.log(randomArray[i]);
 }
 
-for(let i =0;i<randomArray.length;i++){
-    for(let k=i+1;k<randomArray.length-1;k++){
-        if(randomArray[i]==randomArray[k]){
-            repeatingArray[i]=randomArray[i];
-            randomArray[i]=randomArray[i+1];
-           // console.log(randomArray[i]);
-        }
-    }
-}
 console.log("sort")
 for(let i=0;i<randomArray.length;i++){
     if(randomArray[i]<randomArray[i+1]){
@@ -29,7 +22,16 @@ for(let i =0;i<randomArray.length;i++){
     console.log(randomArray[i]);
 }
 console.log("repeatingArray : ");
-
+let p=0;
+for(let j=0;j<randomArray.length;j++){
+    for(let k=j+1;k<randomArray.length;k++){
+        if(randomArray[j]==randomArray[k]){
+            repeatingArray[p]=randomArray[k];
+            p++;
+        }
+        
+    }
+}
 for(let i =0;i<repeatingArray.length;i++){
     console.log(repeatingArray[i]);
 }
