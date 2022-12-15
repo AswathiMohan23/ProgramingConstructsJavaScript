453cb55c46c77e39265fdec86d355270e1edc828
AswathiMohan23 <achu999here@gmail.com>
2022-12-16 02:05:17 +0530

    [REFACTORED] : added comments to the code

--- DayAndMonth.js
+++ DayAndMonth.js
@@ -3,13 +3,13 @@
 //day of month is between March 20 and June 20, false otherwise.
 // enter the input along with file name in the terminal eg :  node filename.js 2 5
 
-const inputFromConsole = process.argv; // Create a constant ‘argvs’ and set it to process.argv properties: 
+const inputFromConsole = process.argv; // Create a constant inputFromConsole and set it to process.argv properties: 
                                     //This will contain the arguments that the user passes in the command line.
 
 
 //Since the first two arguments passed in the command line are irrelevant to the program because
 // the first one is ‘node’ itself since we use it to run the application and second is the name of the file 
-//which we want it to execute in our case its ‘app.js’, so we have to remove it by using slice method.
+//which we want it to execute in our case its fileName.js’, so we have to remove it by using slice method.
 
 const argv = inputFromConsole.slice(2);
 const day=  parseInt(argv[0]);
