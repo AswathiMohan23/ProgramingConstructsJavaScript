1b52b1c534df2817dd41ba0d175abe5cbada8637
AswathiMohan23 <achu999here@gmail.com>
2022-12-16 02:24:19 +0530

    [ADDED] : code to generate random head and tail

---
+++ flipCoin.js
@@ -0,0 +1,11 @@
+for(let i=0;i<=5;i++){
+    let random=Math.floor(Math.random() * 2)+1;
+    switch(random){
+        case 1 : console.log("Tail");
+                 break;
+        case 2 : console.log("Head");
+                 break;     
+    }
+    
+}
+
