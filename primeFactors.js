e873adfd4f306af33cf837981e57aa5c4e251561
AswathiMohan23 <achu999here@gmail.com>
2022-12-16 15:24:09 +0530

    [ADDED] : code to find the prime factors of a number

---
+++ primeFactors.js
@@ -0,0 +1,16 @@
+const inputFromConsole = process.argv;
+const argv = inputFromConsole.slice(2);
+let number=  parseInt(argv[0]);
+
+console.log("Prime factors are : ");
+for(let i = 2; i< number; i++) {
+    while(number%i == 0) {
+       console.log(i+" ");
+       number = number/i;
+    }
+ }
+ if(number >2) {
+    console.log(number);
+ }
+
+ //eg : 10 =2,5;
